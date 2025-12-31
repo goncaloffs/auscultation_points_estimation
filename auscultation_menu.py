aded598e432f8a77a37ee902e1c405aec8aeeaca
Gonçalo Fernandes <168077924+goncaloffs@users.noreply.github.com>
2025-12-31 00:17:00 +0000

    Update auscultation_menu.py

--- auscultation_menu.py
+++ auscultation_menu.py
@@ -58,16 +58,14 @@ image_path3 = r'C:\path\to\your\Images\auscultacao_posterior.png'
 image3 = Image.open(image_path3)
 tk_image3 = ImageTk.PhotoImage(image3)
 
-# Create buttons for each file in the main menu frame
-file1_button = ttk.Button(main_menu_frame, text="Cardiac Auscultation", command=lambda: button_clicked(r'path\to\your\cardiac_pulmonar_ausc.py', loading_label), style="TButton")
-file2_button = ttk.Button(main_menu_frame, text="Anterior Auscultation", command=lambda: button_clicked(r'path\to\your\anterior_pulmonar_ausc.py', loading_label), style="TButton")
-file3_button = ttk.Button(main_menu_frame, text="Posterior Auscultation", command=lambda: button_clicked(r'path\to\your\posterior_pulmonar_ausc.py', loading_label), style="TButton")
+file1_button = ttk.Button(main_menu_frame, text="Cardiac Auscultation", command=lambda: button_clicked(r'C:\path\to\your\cardiac_pulmonar_ausc.py', loading_label), style="TButton")
+file2_button = ttk.Button(main_menu_frame, text="Anterior Auscultation", command=lambda: button_clicked(r'C:\path\to\your\anterior_pulmonar_ausc.py', loading_label), style="TButton")
+file3_button = ttk.Button(main_menu_frame, text="Posterior Auscultation", command=lambda: button_clicked(r'C:\path\to\your\posterior_pulmonar_ausc.py', loading_label), style="TButton")
 
 file1_button.grid(row=1, column=0, pady=10, padx=10)
 file2_button.grid(row=1, column=1, pady=10, padx=10)
 file3_button.grid(row=1, column=2, pady=10, padx=10)
 
-# Create a label to display the image
 image_label1 = tk.Label(main_menu_frame, image=tk_image1)
 image_label1.grid(row=2, column=0, pady=10)
 
@@ -77,11 +75,9 @@ image_label2.grid(row=2, column=1, pady=10)
 image_label3 = tk.Label(main_menu_frame, image=tk_image3)
 image_label3.grid(row=2, column=2, pady=10)
 
-# Create a label for the instruction text
 instruction_label = tk.Label(main_menu_frame, text="Choose between the three auscultation modes:", font=('Helvetica', 14), pady=10)
 instruction_label.grid(row=0, column=0, columnspan=3)
 
-# Load your information icon using Pillow
 info_icon_path1 = r'C:\path\to\your\Images\info_icon-removebg-preview.png'
 info_icon1 = Image.open(info_icon_path1)
 info_icon1 = info_icon1.resize((40, 40), Image.LANCZOS)
@@ -97,7 +93,6 @@ info_icon3 = Image.open(info_icon_path3)
 info_icon3 = info_icon3.resize((40, 40), Image.LANCZOS)
 tk_info_icon3 = ImageTk.PhotoImage(info_icon3)
 
-# Create labels for the information icons
 info_icon_label1 = tk.Label(main_menu_frame, image=tk_info_icon1)
 info_icon_label1.grid(row=3, column=0, pady=10, padx=10)
 info_icon_label1.bind("<Button-1>", lambda event: show_info_popup("During cardiac auscultation, the patient usually rests in the supine position (on their back), while the examiner stands to the right. The process begins in the aortic area (second right intercostal space), gradually progressing to the pulmonary area (second left intercostal space). It then moves to the left sternal border until it reaches the tricuspid area (lower left edge of the sternum) and finally to the mitral area (apex of the heart). The examiner should use both the diaphragm and the bell of the stethoscope to pick up various heart sounds."))
@@ -126,3 +121,4 @@ root.configure(background='#f0f0f0')
 root.attributes('-alpha', True)
 
 root.mainloop()
+
